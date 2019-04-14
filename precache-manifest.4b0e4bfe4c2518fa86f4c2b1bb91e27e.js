54ff19a86aac50a1e33a3651baacec77b7621aa3
JeanCarrillo <jeancarrillo@gmail.com>
2019-04-14 16:26:15 +0200

    Updates

---
+++ precache-manifest.4b0e4bfe4c2518fa86f4c2b1bb91e27e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0720a5cb8b3310e58d3381383473b66",
+    "url": "/typing-game/static/media/zombie2.f0720a5c.png"
+  },
+  {
+    "revision": "abdbe1adb4b158991219d64ce616cd5d",
+    "url": "/typing-game/static/media/dirt.abdbe1ad.jpg"
+  },
+  {
+    "revision": "8f00f9413371c9e24b562ef70240f424",
+    "url": "/typing-game/static/media/character-left.8f00f941.png"
+  },
+  {
+    "revision": "cf4384a36580920bf2cb",
+    "url": "/typing-game/static/js/runtime~main.8bbbebfd.js"
+  },
+  {
+    "revision": "baa6e912e6183cd5cc91",
+    "url": "/typing-game/static/js/main.5430137f.chunk.js"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/typing-game/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "baa6e912e6183cd5cc91",
+    "url": "/typing-game/static/css/main.f775d5d8.chunk.css"
+  },
+  {
+    "revision": "0b70bd0f39c01ef950f488c4d9967074",
+    "url": "/typing-game/index.html"
+  }
+];
